6339abff7710218761431d153bf746804eb606d9
Jc Leon <leonjuan123@yahoo.com>
2021-07-13 15:55:24 -0700

    updated image source

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 /* global data */
 /* exported data */
+var $input = document.querySelector('.photo__url');
+var $img = document.querySelector('.img__holder');
+
+var $imgSrc = './images/placeholder-image-square.jpg';
+
+$input.addEventListener('input', function (event) {
+  if ($input.value === '') {
+    $img.src = $imgSrc;
+  } else {
+    $img.src = $input.value;
+  }
+});
+var $submit = document.querySelector('button');
+
+$submit.addEventListener('click', function (event) {
+  // console.log($submit);
+});

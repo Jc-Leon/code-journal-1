7cc31659812d3717d6f960e058ed4c8b93169005
Jc Leon <leonjuan123@yahoo.com>
2021-07-14 10:05:19 -0700

    update object

--- js/main.js
+++ js/main.js
@@ -5,6 +5,15 @@ var $img = document.querySelector('.img__holder');
 
 var $imgSrc = './images/placeholder-image-square.jpg';
 
+var obj = {
+  titleOf: '',
+  photUrl: '',
+  notesOf: '',
+  entry: 0
+};
+
+var entries = [];
+
 $input.addEventListener('input', function (event) {
   if ($input.value === '') {
     $img.src = $imgSrc;
@@ -12,8 +21,22 @@ $input.addEventListener('input', function (event) {
     $img.src = $input.value;
   }
 });
-var $submit = document.querySelector('button');
+var form = document.querySelector('form');
 
-$submit.addEventListener('click', function (event) {
-  // console.log($submit);
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  var newObj = {
+    ...obj,
+    titleOf: form.title.value,
+    photUrl: form.url.value,
+    notesOf: form.notes.value,
+    entry: obj.entry++
+  };
+
+  entries.push(newObj);
+
+  $img.src = $imgSrc;
+  document.getElementById('log').reset();
+  // console.log(entries);
 });

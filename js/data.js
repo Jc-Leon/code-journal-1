bc8e5061e257754d49cc3c2ba1611a729471bd5e
Jc Leon <leonjuan123@yahoo.com>
2021-07-14 17:22:35 -0700

    update progress

--- js/data.js
+++ js/data.js
@@ -6,3 +6,25 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var form = document.querySelector('form');
+
+form.addEventListener('submit', function newObj() {
+  event.preventDefault();
+
+  var newObj = {
+    titleOf: form.title.value,
+    photUrl: form.url.value,
+    notesOf: form.notes.value,
+    entry: data.nextEntryId++
+  };
+
+  data.entries.unshift(newObj);
+
+  localStorage.setItem('Values', JSON.stringify(data.entries));
+  var objects = localStorage.getItem('Values');
+  if (objects !== null) {
+    data.entries = JSON.parse(objects);
+  }
+
+});
--- js/main.js
+++ js/main.js
@@ -1,48 +1,20 @@
 /* global data */
 /* exported data */
-var $input = document.querySelector('.photo__url');
-var $img = document.querySelector('.img__holder');
-
+var $input = document.querySelector('.photo-url');
+var $img = document.querySelector('.img-holder');
+var form = document.querySelector('form');
 var $imgSrc = './images/placeholder-image-square.jpg';
 
-var obj = {
-  titleOf: '',
-  photUrl: '',
-  notesOf: '',
-  entry: 0
-};
-
-var entries = [];
-
-$input.addEventListener('input', function (event) {
+$input.addEventListener('input', function imageSrc(event) {
   if ($input.value === '') {
     $img.src = $imgSrc;
   } else {
     $img.src = $input.value;
   }
 });
-var form = document.querySelector('form');
 
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  var newObj = {
-    titleOf: form.title.value,
-    photUrl: form.url.value,
-    notesOf: form.notes.value,
-    entry: obj.entry++
-  };
-
-  entries.unshift(newObj);
-
-  localStorage.setItem('Values', JSON.stringify(entries));
-  var objects = localStorage.getItem('Values');
-  if (objects !== null) {
-    entries = JSON.parse(objects);
-  }
-  // console.log(JSON.parse(objects));
-
+form.addEventListener('submit', function resetForm() {
   $img.src = $imgSrc;
-  document.getElementById('log').reset();
-  // console.log(newObj);
+
+  form.reset();
 });
